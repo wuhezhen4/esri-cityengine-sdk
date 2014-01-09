9cd8e1a36187d72d8958b7d3efe3b3ebbaff1ca2
Simon Haegler <shaegler@esri.com>
2014-01-09 15:21:39 +0100

    cosmetic changes

--- examples/prt4cmd/src/prt4cmd.cpp
+++ examples/prt4cmd/src/prt4cmd.cpp
@@ -2,10 +2,24 @@
  * Esri CityEngine SDK CLI Example
  *
  * This example demonstrates the main functionality of the Procedural Runtime API.
+ *
+ * See README.md in http://github.com/ArcGIS/esri-cityengine-sdk for build instructions.
+ *
  * Written by Matthias Specht and Simon Haegler
  * Esri R&D Center Zurich, Switzerland
  *
- * See http://github.com/ArcGIS/esri-cityengine-sdk for instructions.
+ * Copyright 2014 Esri R&D Center Zurich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <cstdio>
